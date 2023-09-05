95f4be5fadffa10966f82cee5d5ff8aa0e5887ec
ByOnez <118693852+ByOnez@users.noreply.github.com>
2023-09-04 22:04:18 -0300

    conclusão data.js

--- src/data.js
+++ src/data.js
@@ -1,25 +1,50 @@
+let formData = {}
+
 document.addEventListener("DOMContentLoaded", function () {
+    let button = document.getElementById("button-send")
+    if (button !== null) {
+        button.addEventListener("click", function () {
+            formData = getForm()
+            formInfo(formData)
+        })
+    } else {
+        console.log("Erro")
+    }
+})
+
+function getForm() {
     let nameInput = document.getElementById("input-name")
     let emailInput = document.getElementById("input-email")
     let titleInput = document.getElementById("input-title")
     let descriptionTxtarea = document.getElementById("textarea-description")
-    let button = document.getElementById("button-send")
 
-    if (button !== null) {
-        button.addEventListener("click", getForm)
-    } else {
-        console.log("Erro")
+    let nameValue = nameInput.value
+    let emailValue = emailInput.value
+    let titleValue = titleInput.value
+    let descriptionValue = descriptionTxtarea.value
+
+    nameValidate(nameInput)
+    emailValidate(emailInput)
+    titleValidate(titleInput)
+
+    return {
+        name: nameValue,
+        email: emailValue,
+        title: titleValue,
+        description: descriptionValue,
     }
+}
 
-    function getForm() {
-        let name = nameInput.value
-        let email = emailInput.value
-        let title = titleInput.value
-        let description = descriptionTxtarea.value
+function formInfo(formData) {
+    let name = formData.name
+    let email = formData.email
+    let title = formData.title
+    let description = formData.description
 
-        nameValidate(nameInput)
-        emailValidate(emailInput)
-        titleValidate(titleInput)
-        descriptionValidate(descriptionTxtarea)
-    }
-})
+    console.log(name)
+    console.log(email)
+    console.log(title)
+    console.log(description)
+}
+
+export { formInfo }
