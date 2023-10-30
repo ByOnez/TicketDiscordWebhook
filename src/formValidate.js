aaa3777e94265c3c1bd51265b5f8bdcfcb29fa8d
ByOnez <118693852+ByOnez@users.noreply.github.com>
2023-10-29 21:51:01 -0300

    update formvalidate.js

--- src/formValidate.js
+++ src/formValidate.js
@@ -1,32 +1,47 @@
-let nameInput = document.getElementById("input-name")
-let emailInput = document.getElementById("input-email")
-let titleInput = document.getElementById("input-title")
-let descriptionTxtarea = document.getElementById("textarea-description")
+let nameInput = document.getElementById("input-name");
+let emailInput = document.getElementById("input-email");
+let titleInput = document.getElementById("input-title");
+let descriptionTxtarea = document.getElementById("textarea-description");
 
 function nameValidate(nameInput) {
     if (nameInput.value.length < 3) {
-        nameInput.classList.add("error-transition")
+        nameInput.classList.remove("success-border");
+        nameInput.classList.add("error-border");
     } else {
-        nameInput.classList.add("sucess-transition")
+        nameInput.classList.remove("error-border");
+        nameInput.classList.add("success-border");
     }
 }
 
 function emailValidate(emailInput) {
-    let email = emailInput.value
-    const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-    let emailTest = emailRegex.test(email)
+    let email = emailInput.value;
+    const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    let emailTest = emailRegex.test(email);
     if (emailTest === false) {
-        emailInput.classList.add("error-transition")
+        emailInput.classList.remove("success-border");
+        emailInput.classList.add("error-border");
     } else {
-        emailInput.classList.add("sucess-transition")
+        emailInput.classList.remove("error-border");
+        emailInput.classList.add("success-border");
     }
 }
 
 function titleValidate(titleInput) {
-    let title = titleInput.value
-    if (title === "") {
-        titleInput.classList.add("error-transition")
+    if (titleInput.value === "") {
+        titleInput.classList.remove("success-border");
+        titleInput.classList.add("error-border");
     } else {
-        titleInput.classList.add("sucess-transition")
+        titleInput.classList.remove("error-border");
+        titleInput.classList.add("success-border");
+    }
+}
+
+function descriptionValidate(descriptionTxtarea) {
+    if (descriptionTxtarea.value.length > 4000) {
+        descriptionTxtarea.classList.remove("success-border");
+        descriptionTxtarea.classList.add("error-border");
+    } else {
+        descriptionTxtarea.classList.remove("error-border");
+        descriptionTxtarea.classList.add("success-border");
     }
 }
