2a324b1fb60f74835250d94e77e168f8c7435ac3
Enzo <enzolupki.dev@gmail.com>
2025-05-11 22:08:42 -0300

    fix: dotenv

--- src/services/sendToDiscord.ts
+++ src/services/sendToDiscord.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { FormValues } from "../types/Ticket";
-
-const webhookURL = "https://discord.com/api/webhooks/1264439650773110876/FBe3-EgaR4FBMQmjwRaZsUibmCJc-LDmAKkNPbAp6-am0NzkEtz92kVKBXlnMJ_Lesyx"; 
+import dotenv from "dotenv";
+dotenv.config();
 
 export const sendToDiscord = async (data: FormValues) => {
     try {    
-        await axios.post(webhookURL, {
+        await axios.post(process.env.WEBHOOK as string, {
             content: `**Novo Ticket Recebido:**\n**Nome:** ${data.name}\n**Email:** ${data.email}\n**Título do Ticket:** ${data.title}\n**Descrição:** ${data.description}`
           });
     } catch (error) {
